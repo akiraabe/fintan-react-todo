b9acbb1b3be7659afa3ce80c1e2842edc8e69aa0
Akira Abe <abe.akira@tis.co.jp>
2021-02-02 13:49:54 +0900

    useEffectで静的データを設定しました。

--- frontend/src/components/TodoBoard.tsx
+++ frontend/src/components/TodoBoard.tsx
@@ -1,27 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./TodoBoard.css";
 import { TodoFilter } from "./TodoFilter";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 
 type Todo = {
-    id: number
-    text: string
-    completed: boolean
-}
+  id: number;
+  text: string;
+  completed: boolean;
+};
 
 export const TodoBoard: React.FC = () => {
-    const [todos] = useState<Todo[]>([
-        { id: 2001, text: '洗い物をする', completed: true },
-        { id: 2002, text: '洗濯物を干す', completed: false },
-        { id: 2003, text: '買い物へ行く', completed: false },
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    setTodos([
+      { id: 2001, text: "洗い物をする", completed: true },
+      { id: 2002, text: "洗濯物を干す", completed: false },
+      { id: 2003, text: "買い物へ行く", completed: false },
+      { id: 2004, text: "Fintanハンズオンやる", completed: false },
     ]);
+  }, []);
 
   return (
     <div className="TodoBoard_content">
       <TodoForm />
       <TodoFilter />
-          <TodoList todos={todos} />
+      <TodoList todos={todos} />
     </div>
   );
 };
